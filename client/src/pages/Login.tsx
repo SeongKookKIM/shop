92026e720a3fd20d88b17990262ea28ec0909b7f
SeongKookKIM <ksk8646@naver.com>
2023-11-22 21:16:18 +0900

    로그인페이지 진행중

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 function Login() {
-  return <div className="login">Login</div>;
+  return (
+    <div className="login">
+      <div className="login-wrapper">
+        <span>로그인</span>
+        <form>
+          <label>이메일계정</label>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
