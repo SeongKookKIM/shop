9e1047d9526bc457fc0d26d45912434bb40fd94f
SeongKookKIM <ksk8646@naver.com>
2023-11-23 21:45:07 +0900

    server(Login.js)태그 정리

--- routes/login/Login.js
+++ routes/login/Login.js
@@ -12,9 +12,9 @@ const MongoClient = require("mongodb").MongoClient;
 MongoClient.connect(
   process.env.MONGO,
   { useUnifiedTopology: true },
-  (에러, client) => {
-    if (에러) {
-      return console.log(에러);
+  (err, client) => {
+    if (err) {
+      return console.log(err);
     }
     db = client.db("shop");
   }
@@ -35,19 +35,11 @@ router.use(
 router.use(passport.initialize());
 router.use(passport.session());
 
-function loginConfrim(요청, 응답, next) {
-  if (요청.user) {
-    next(); //통과
+router.post("/", passport.authenticate("local"), function (req, res) {
+  if (req.user) {
+    return res.status(200).json(req.user);
   } else {
-    응답.send("로그인을 안하셨습니다.");
-  }
-}
-
-router.post("/", passport.authenticate("local"), function (요청, 응답) {
-  if (요청.user) {
-    return 응답.status(200).json(요청.user);
-  } else {
-    return 응답.status(401).json({ message: "로그인 실패" });
+    return res.status(401).json({ message: "로그인에 실패하였습니다." });
   }
 });
 
@@ -60,25 +52,22 @@ passport.use(
       session: true,
       passReqToCallback: false,
     },
-    function (입력한아이디, 입력한비번, done) {
-      db.collection("user").findOne(
-        { email: 입력한아이디 },
-        function (에러, 결과) {
-          if (에러) return done(에러);
+    function (enterEmail, enterPassword, done) {
+      db.collection("user").findOne({ email: enterEmail }, function (err, res) {
+        if (err) return done(err);
 
-          if (!결과) {
-            return done(null, false, { message: "존재하지않는 이메일입니다." });
+        if (!res) {
+          return done(null, false, { message: "존재하지않는 이메일입니다." });
+        } else {
+          const result = bcrypt.compareSync(enterPassword, res.password);
+
+          if (result) {
+            return done(null, res);
           } else {
-            const result = bcrypt.compareSync(입력한비번, 결과.password);
-
-            if (result) {
-              return done(null, 결과);
-            } else {
-              return done(null, false, { message: "비밀번호가 틀렸습니다." });
-            }
+            return done(null, false, { message: "비밀번호가 틀렸습니다." });
           }
         }
-      );
+      });
     }
   )
 );
@@ -87,9 +76,9 @@ passport.serializeUser(function (user, done) {
   done(null, user.email);
 });
 
-passport.deserializeUser(function (아이디, done) {
-  db.collection("user").findOne({ email: 아이디 }, function (에러, 결과) {
-    done(null, 결과);
+passport.deserializeUser(function (enterEmail, done) {
+  db.collection("user").findOne({ email: enterEmail }, function (err, result) {
+    done(null, result);
   });
 });
 
