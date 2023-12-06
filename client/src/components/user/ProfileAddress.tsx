9ed23af4024a67eebb6c39ff35b881daec05749a
SeongKookKIM <ksk8646@naver.com>
2023-12-06 16:09:20 +0900

    프로필정보 및 수정하기(컴포넌트로 목록 나눔)

---
+++ client/src/components/user/ProfileAddress.tsx
@@ -0,0 +1,128 @@
+import React, { useEffect, useState } from "react";
+import { UserType } from "../../type/Type";
+import { useDaumPostcodePopup } from "react-daum-postcode";
+import axios from "axios";
+import { LuArrowLeft } from "react-icons/lu";
+
+interface UserPropsType {
+  userInfo: UserType | undefined;
+  setProfileEdit: React.Dispatch<React.SetStateAction<string>>;
+}
+
+function ProfileAddress({ userInfo, setProfileEdit }: UserPropsType) {
+  const [addressEdit, setAddressEdit] = useState<string>("");
+  const [addressDetailEdit, setAddressDetailEdit] = useState<string>("");
+
+  useEffect(() => {
+    if (userInfo) {
+      setAddressEdit(userInfo.address);
+      setAddressDetailEdit(userInfo.addressdetail);
+    }
+  }, []);
+
+  // 주소API
+  const open = useDaumPostcodePopup(
+    "https://t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js"
+  );
+
+  const handleComplete = (data: any) => {
+    let fullAddress = data.address;
+    let extraAddress = "";
+
+    if (data.addressType === "R") {
+      if (data.bname !== "") {
+        extraAddress += data.bname;
+      }
+      if (data.buildingName !== "") {
+        extraAddress +=
+          extraAddress !== "" ? `, ${data.buildingName}` : data.buildingName;
+      }
+      fullAddress += extraAddress !== "" ? ` (${extraAddress})` : "";
+    }
+
+    setAddressEdit(fullAddress);
+  };
+
+  const handlerFindAddress = () => {
+    let popupWidth = 500;
+    let popupHeight = 600;
+    let popupX = Math.round(window.screen.width / 2 - popupWidth / 2);
+    let popupY = Math.round(window.screen.height / 2 - popupHeight / 2);
+    open({
+      onComplete: handleComplete,
+      width: popupWidth,
+      height: popupHeight,
+      top: popupY,
+      left: popupX,
+    });
+  };
+
+  const handlerAddressEdit = () => {
+    if (addressEdit === "") {
+      alert("주소를 입력해주세요.");
+    } else {
+      if (window.confirm("주소를 변경하시겠습니까?")) {
+        axios
+          .post("http://localhost:8080/profile/edit/address", {
+            _id: userInfo?._id,
+            address: addressEdit,
+            addressdetail: addressDetailEdit,
+          })
+          .then((res) => {
+            alert(res.data);
+
+            axios
+              .post("http://localhost:8080/profile", { _id: userInfo?._id })
+              .then((res) => {
+                localStorage.removeItem("user");
+
+                localStorage.setItem("user", JSON.stringify(res.data));
+
+                window.location.reload();
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      } else {
+        return;
+      }
+    }
+  };
+  return (
+    <div className="profile-edit-address">
+      <LuArrowLeft
+        className="back"
+        onClick={() => {
+          setProfileEdit("all");
+        }}
+      />
+      <p>주소</p>
+      <input
+        type="text"
+        name="address"
+        value={addressEdit}
+        onChange={(e) => {
+          setAddressEdit(e.target.value);
+        }}
+      ></input>
+      <input
+        type="text"
+        name="address"
+        value={addressDetailEdit}
+        onChange={(e) => {
+          setAddressDetailEdit(e.target.value);
+        }}
+      ></input>
+      <div className="change-btn">
+        <span onClick={handlerFindAddress}>주소찾기</span>
+        <span onClick={handlerAddressEdit}>변경하기</span>
+      </div>
+    </div>
+  );
+}
+
+export default ProfileAddress;
--- routes/user/Profile.js
+++ routes/user/Profile.js
@@ -3,6 +3,8 @@ let router = require("express").Router();
 //ENV 파일
 require("dotenv").config();
 
+const bcrypt = require("bcrypt");
+
 // Mongo DB
 let db;
 const MongoClient = require("mongodb").MongoClient;
@@ -25,6 +27,116 @@ router.post("/", (req, res) => {
       if (err) console.log(err);
 
       return res.status(200).json(result);
+    }
+  );
+});
+
+router.post("/delete", (req, res) => {
+  db.collection("user").deleteOne(
+    { _id: ObjectId(req.body._id) },
+    (err, result) => {
+      if (err) console.log(err);
+
+      return res.status(200).send("귀하의 계정을 삭제하였습니다.");
+    }
+  );
+});
+
+router.post("/edit/name", (req, res) => {
+  db.collection("user").updateOne(
+    { _id: ObjectId(req.body._id) },
+    { $set: { name: req.body.name } },
+    (err, result) => {
+      if (err) console.log(err);
+
+      return res.status(200).send("이름을 변경하셨습니다.");
+    }
+  );
+});
+
+router.post("/edit/address", (req, res) => {
+  db.collection("user").updateOne(
+    { _id: ObjectId(req.body._id) },
+    {
+      $set: {
+        address: req.body.address,
+        addressdetail: req.body.addressdetail,
+      },
+    },
+    (err, result) => {
+      if (err) console.log(err);
+
+      return res.status(200).send("주소를 변경하셨습니다.");
+    }
+  );
+});
+
+router.post("/edit/email", (req, res) => {
+  db.collection("user").updateOne(
+    { _id: ObjectId(req.body._id) },
+    {
+      $set: {
+        email: req.body.email,
+      },
+    },
+    (err, result) => {
+      if (err) console.log(err);
+
+      return res.status(200).send("이메일을 변경하셨습니다.");
+    }
+  );
+});
+
+router.post("/edit/phone", (req, res) => {
+  db.collection("user").updateOne(
+    { _id: ObjectId(req.body._id) },
+    {
+      $set: {
+        phone: req.body.phone,
+      },
+    },
+    (err, result) => {
+      if (err) console.log(err);
+
+      return res.status(200).send("연락처를 변경하셨습니다.");
+    }
+  );
+});
+
+router.post("/edit/password", (req, res) => {
+  const hashed = bcrypt.hashSync(req.body.password, 10);
+
+  db.collection("user").updateOne(
+    { _id: ObjectId(req.body._id) },
+    {
+      $set: {
+        password: hashed,
+      },
+    },
+    (err, result) => {
+      if (err) console.log(err);
+
+      return res.status(200).send("비밀번호를 변경하셨습니다.");
+    }
+  );
+});
+
+router.post("/edit/password/confirm", (req, res) => {
+  db.collection("user").findOne(
+    { _id: ObjectId(req.body._id) },
+    async (err, result) => {
+      if (err) console.log(err);
+
+      const passwordMatch = await bcrypt.compare(
+        req.body.password,
+        result.password
+      );
+
+      if (passwordMatch) {
+        return res.status(200).send("비밀번호 확인");
+      } else {
+        return res.status(403).send("비밀번호가 틀렸습니다");
+      }
     }
   );
 });
